993dafa85cb6970187f06c43dfef3c187d1a3b49
Joy <you@example.com>
2022-03-03 15:47:55 +0530

    Fourth COmmit

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-const koa = require('koa')
-const koarouter= require('koa-router')
-const parser= require('koa-bodyparser')
-const mongodb = require('../Blog/Database/db')
-const authRoute = require('./Router/route.js')
 
-const app = new koa()
+const app = require('..app/')
 
-app.use(parser())
 
-app.use(authRoute.routes()).use(authRoute.allowedMethods())
 
 app.listen(2000,()=>{
     console.log("Port is running")
 })
 
-
-
-//module.exports= koa,koarouter,parser,mongodb;
